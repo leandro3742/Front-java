972cc63d5d56decb8a52431ff56bd75fd6041f40
CintiaLeal <cintialeal31m@gmail.com>
2022-07-20 11:24:32 -0300

    editar y eliminar listo

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,8 @@ import BloquearUsuario from "./views/admin/bloquearUsuario";
 import DesbloquearUsuario from "./views/admin/desbloquearUsuario";
 import Reportes from "./views/generador/reportes";
 import AltaPersona from "./views/admin/altaPersona";
+import EliminarPersona from "./views/admin/eliminarPersona";
+import EditarPersona from "./views/admin/editarPersona";
 function App() {
   const [isLogged, setIsLogged] = useState(false);
   const [tipoUsuario, setTipoUsuario] = useState(false);
@@ -72,8 +74,11 @@ function App() {
             <Route path="/admin/verContenido/:id" element={<VerContenido />} />
             <Route path="/admin/gestionarContenido" element={<GestionarContenido />} />
             <Route path="/admin/todoContenido" element={<TodoContenido />} />
+          
             <Route path="/admin/contenidoDisponible" element={<ContenidoDisponible />} />
             <Route path="/admin/altaPersona" element={<AltaPersona />} />
+            <Route path="/admin/eliminarPersona" element={<EliminarPersona />} />
+            <Route path="/admin/editarPersona/:id" element={<EditarPersona />} />            
             <Route path="/admin/bloquearUsuario" element={<BloquearUsuario />} />
             <Route path="/admin/desbloquearUsuario" element={<DesbloquearUsuario />} />
             <Route path="/generador/" element={<HomeGenerador />} />
