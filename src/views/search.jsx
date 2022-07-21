6f2c6ffd8663c3ce476c7a3a58111d84d1674c76
leandro3742 <leandro.marrero@outlook.com>
2022-07-21 00:09:16 -0300

    buscar por nombre de contenidos creados

--- src/views/search.jsx
+++ src/views/search.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+const fetchBuscar = async(url) => {
+    const response = await fetch(url)
+    if(response.status < 300)
+        return await response.json();
+    return "ERROR"
+}
 function Search() {
+    const [buscar, setBuscar] = useState("");
+    const [contenidos, setContenidos] = useState([])
+    const [imageSize, setImageSize] = useState({});
+
+    useEffect(() => {
+        fetchBuscar(`http://localhost:8080/contenidos/buscarPorNombre/${buscar}`)
+        .then(res => {
+            if(res !== "ERROR")
+                setContenidos(res)
+        })
+        if (window.screen.width > 730) {
+            setImageSize({ maxHeight: "175px", maxWidth: "300px" });
+        } else {
+            setImageSize({ maxHeight: "175px", maxWidth: "150px" });
+        }
+    }, [buscar]);
+
     const styleDiv = {
         backgroundColor: "#505360",
         height: "50px",
@@ -10,8 +34,22 @@ function Search() {
         fontSize: "23px",
     };
     return (
-        <div className="d-flex justify-content-center mt-2 ">
-            <input style={styleDiv} className="texto d-flex justify-content-center align-items-center rounded text-center" placeholder="Título o género" />
+        <div className="d-flex flex-column justify-content-center mt-2 ">
+            <input style={styleDiv} className="texto d-flex justify-content-center align-items-center rounded text-center" onChange={(e)=>setBuscar(e.target.value)} placeholder="Título o género" />
+            {contenidos.length > 0 
+                ?
+                <div>
+                {contenidos.map((elem, index) => {
+                    return (
+                        <div style={imageSize} key={index} className="m-2">
+                            <Link to={`/detail/${elem.id}`} ><img style={imageSize} className="frontPage rounded" src={elem.fotoPortada} key={index} /></Link>
+                            <span>{elem.descripcion}</span>
+                        </div>
+                    )
+                })}
+                </div>
+                : <span>No se encontraron contenidos con ese nombre</span>
+            }
         </div>
     );
 }
